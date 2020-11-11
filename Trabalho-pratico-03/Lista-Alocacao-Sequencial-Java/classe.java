13003716c563530996f0ba20121b9bd3c8f2c5a2
SamCambraia1 <cambraia_samir@hotmail.com>
2020-11-11 16:18:06 -0300

    Lista alocacao sequencial em java

--- Trabalho-pratico-03/Lista-Alocacao-Sequencial-Java/classe.java
+++ Trabalho-pratico-03/Lista-Alocacao-Sequencial-Java/classe.java
@@ -1,16 +1,128 @@
 import java.io.*;
 import java.util.*;
 
+class Lista {
 
+    private Player[] array;
+    private int n;
 
-class Player{
-  
-    private int  id;           private String universidade;
-    private int altura;        private String nome;
-    private int peso;          private String cidadeNascimento; 
-    private int anoNascimento; private String estadoNacimento;
-    
-    public Player(){
+    public Lista() {
+        this(1000);
+    }
+
+    public Lista(int tamanho) {
+        array = new Player[tamanho];
+        int n = 0;
+
+    }// end construct Lista
+
+    public void inserirInicio(Player x) throws Exception {
+        if (n >= array.length) {
+            throw new Exception("Erro ao inserir");
+        } // end if
+        for (int i = n; i > 0; i--) {
+            array[i] = array[i - 1];
+        }
+
+        array[0] = x;
+        n++;
+    }// end inserirInicio
+
+    public void inserirFim(Player x) throws Exception {
+        if (n >= array.length) {
+            throw new Exception("erro ao inserir");
+        } // end if
+
+        array[n] = x;
+        n++;
+    }// end inserirFim
+
+    public void inserir(Player x, int pos) throws Exception {
+        if (n >= array.length || pos < 0 || pos > n) {
+            throw new Exception("Erro ao inserir");
+        } // end if
+        for (int i = n; i > pos; i--) {
+            array[i] = array[i - 1];
+        }
+
+        array[pos] = x;
+        n++;
+    }// end inserir
+
+    public Player removerInicio() throws Exception {
+
+        if (n == 0) {
+            throw new Exception("Erro ao remover Inicio");
+        } // end if
+
+        Player saida = array[0];
+        n--;
+
+        for (int i = 0; i < n; i++) {
+            array[i] = array[i + 1];
+        } // end for
+
+        return saida;
+    }// end removerInicio
+
+    public Player removerFim() throws Exception {
+
+        if (n == 0) {
+            throw new Exception("Erro ao remover Fim");
+
+        } // end if
+
+        Player saida = array[--n];
+        return saida;
+    }// end removerFim
+
+    public Player remover(int pos) throws Exception {
+
+        // validar remocao
+        if (n == 0 || pos < 0 || pos >= n) {
+            throw new Exception("Erro ao remover!");
+        }
+
+        Player resp = array[pos];
+        n--;
+
+        for (int i = pos; i < n; i++) {
+            array[i] = array[i + 1];
+        }
+
+        return resp;
+    }// end remover
+
+    public void mostrar() {
+        for (int i = 0; i < n; i++) {
+            System.out.print("[" + i + "]");
+            array[i].Imprimir();
+        }
+
+    }// end mostrar
+
+    public boolean pesquisa(Player x) throws Exception {
+        boolean retorno = false;
+        for (int i = 0; i < n && retorno == false; i++) {
+            retorno = (array[i] == x);
+        } // end for
+        return retorno;
+    }// end pesquisa
+
+}// end class Lista
+
+class Player {
+
+    private int id;
+    private String universidade;
+    private int altura;
+    private String nome;
+    private int peso;
+    private String cidadeNascimento;
+    private int anoNascimento;
+    private String estadoNascimento;
+
+    public Player() {
         id = 0;
         altura = 0;
         peso = 0;
@@ -18,138 +130,248 @@ class Player{
         nome = "";
         universidade = "";
         cidadeNascimento = "";
-        estadoNacimento = "";
+        estadoNascimento = "";
 
-    }//end construtor padrao
+    }// end construtor padrao
 
-  
-    public void setId(int id ) {
-        this.id = id;
-    }//end setId
-    public int getId(){
-       return id;
-    }//end getId
-    public void setAltura(int altura){
-       this.altura = altura;
-    }//end setAltura
-    public int getAltura(){
-      return altura;
-    }//end getAltura
-    public void setPeso(int peso){
-       this.peso = peso;
-    }//end setPesoo
-    public int getPeso(){
-        return peso;
-    }//end getPeso
-    public void setAnoNascimento(int anoNascimento){
-        this.anoNascimento = anoNascimento;
-    }//end setAnoNascimento
-    public int getAnoNascimento(){
-        return anoNascimento;
-    }//end getAnoNascimento
-    public void setNome(String nome){
+    public Player(String nome, String universidade, String cidadeNascimento, String estadoNascimento, int id,
+            int altura, int peso, int anoNascimento) {
         this.nome = nome;
-    }//end setNome
-    public String getNome(){
-        return nome;
-    }//end getNome
-    public void setUniversidade(String universidade){
         this.universidade = universidade;
-    }//end setUniversidade
-    public String getUniversidade( ){
-        return universidade;
-    }//end getUniversidade
-    public void setCidadeNascimento(String cidadeNascimento){
         this.cidadeNascimento = cidadeNascimento;
-    }//end setCidadeNascimento
-    public String getCidadeNascimento( ){
+        this.estadoNascimento = estadoNascimento;
+        this.id = id;
+        this.altura = altura;
+        this.peso = peso;
+        this.anoNascimento = anoNascimento;
+
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }// end setId
+
+    public int getId() {
+        return id;
+    }// end getId
+
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }// end setAltura
+
+    public int getAltura() {
+        return altura;
+    }// end getAltura
+
+    public void setPeso(int peso) {
+        this.peso = peso;
+    }// end setPesoo
+
+    public int getPeso() {
+        return peso;
+    }// end getPeso
+
+    public void setAnoNascimento(int anoNascimento) {
+        this.anoNascimento = anoNascimento;
+    }// end setAnoNascimento
+
+    public int getAnoNascimento() {
+        return anoNascimento;
+    }// end getAnoNascimento
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }// end setNome
+
+    public String getNome() {
+        return nome;
+    }// end getNome
+
+    public void setUniversidade(String universidade) {
+        this.universidade = universidade;
+    }// end setUniversidade
+
+    public String getUniversidade() {
+        return universidade;
+    }// end getUniversidade
+
+    public void setCidadeNascimento(String cidadeNascimento) {
+        this.cidadeNascimento = cidadeNascimento;
+    }// end setCidadeNascimento
+
+    public String getCidadeNascimento() {
         return cidadeNascimento;
-    }//end GetCidadeNascimento
-    public void setEstadoDeNascimento(String estadoNascimento){
-        this.estadoNacimento = estadoNascimento;
-    }//end setEstadoDeNascimento
-    public String getEstadoDeNascimento( ){
-       return estadoNacimento;
-    }//end setEstadoDeNascimento
+    }// end GetCidadeNascimento
 
+    public void setEstadoDeNascimento(String estadoNascimento) {
+        this.estadoNascimento = estadoNascimento;
+    }// end setEstadoDeNascimento
 
+    public String getEstadoDeNascimento() {
+        return estadoNascimento;
+    }// end setEstadoDeNascimento
 
+    public Player clone(Player x) {
 
-}//class objeto
-public class classe{ 
-     
-    public static Player[] ply = new Player[1000];
-    public static int coutPlayers = 0; 
-    
-    public static void Imprimir(){
-       System.out.println("["+ply[coutPlayers].getId()+" ## "+ply[coutPlayers].getNome()+" ## "+ply[coutPlayers].getAltura()+" ## "+ply[coutPlayers].getPeso()
-        +" ## "+ ply[coutPlayers].getAnoNascimento()+" ## "+ ply[coutPlayers].getUniversidade()+" ## "+ply[coutPlayers].getCidadeNascimento()+
-        " ## " +ply[coutPlayers].getEstadoDeNascimento()+"]");
+        x.id = id;
+        x.nome = nome;
+        x.altura = altura;
+        x.peso = peso;
+        x.universidade = universidade;
+        x.cidadeNascimento = cidadeNascimento;
+        x.estadoNascimento = estadoNascimento;
+        x.anoNascimento = anoNascimento;
 
-    }//end imprimir
+        return x;
+    }// end func clone
 
+    public void Imprimir() {
 
+        System.out.println(" ## " + getNome() + " ## " + getAltura() + " ## " + getPeso() + " ## " + getAnoNascimento()
+                + " ## " + getUniversidade() + " ## " + getCidadeNascimento() + " ## " + getEstadoDeNascimento()
+                + " ##");
+    } // end imprimir
 
-    public static void tratamentoString(String s )throws Exception { 
+}// class objeto
+
+public class classe {
+
+    public static Lista list = new Lista();
+    public static Player ply = new Player();
+    public static int countPlayers = 0;
+
+    public static void tratamentoString(String s) throws Exception {
         int tam = s.length();
-        if( s.charAt(tam-1) == ',') s = s + "nao informado"; 
-        s = s.replaceAll(",,",",nao informado,");
+        if (s.charAt(tam - 1) == ',')
+            s = s + "nao informado";
+        s = s.replaceAll(",,", ",nao informado,");
         String vetor[] = s.split(",");
-        
-         int id = Integer.parseInt(vetor[0]);
-         ply[coutPlayers].setId(id);
 
-         ply[coutPlayers].setNome(vetor[1]);
+        int id = Integer.parseInt(vetor[0]);
 
-         int altura = Integer.parseInt(vetor[2]);
-         ply[coutPlayers].setAltura(altura);
+        ply.setId(id);
 
-         int peso = Integer.parseInt(vetor[3]);
-         ply[coutPlayers].setPeso(peso);
+        ply.setNome(vetor[1]);
 
-         ply[coutPlayers].setUniversidade(vetor[4]);
+        int altura = Integer.parseInt(vetor[2]);
 
-         int anoNascimento = Integer.parseInt(vetor[5]);
-         ply[coutPlayers].setAnoNascimento(anoNascimento);
+        ply.setAltura(altura);
 
-         ply[coutPlayers].setCidadeNascimento(vetor[6]);
+        int peso = Integer.parseInt(vetor[3]);
 
-         ply[coutPlayers].setEstadoDeNascimento(vetor[7]);
+        ply.setPeso(peso);
 
+        ply.setUniversidade(vetor[4]);
 
-    }//end tratamentoString
+        int anoNascimento = Integer.parseInt(vetor[5]);
 
-    public static String leituraArquivo(String nomeArq)throws Exception{ 
-        
+        ply.setAnoNascimento(anoNascimento);
+
+        ply.setCidadeNascimento(vetor[6]);
+
+        ply.setEstadoDeNascimento(vetor[7]);
+
+    }// end tratamentoString
+
+    public static String leituraArquivo(String nomeArq) throws Exception {
         String linha = "";
         BufferedReader file = new BufferedReader(new InputStreamReader(new FileInputStream("/tmp/players.csv")));
-         
+
         linha = file.readLine();
-        while(linha != null){
-           String[] id = linha.split(",");           
-           if(id[0].equals(nomeArq) == true) return linha;
-           linha = file.readLine();
-        }//end while
-       String s = "";
-       
-       return s;
-    
-    }//end leituraArquivo
-    public static void main(String [] args) throws Exception{
-            
+        while (linha != null) {
+            String[] id = linha.split(",");
+            if (id[0].equals(nomeArq) == true)
+                return linha;
+            linha = file.readLine();
+        } // end while
+        String s = "";
+        System.out.println(s);
+        return s;
+    } // end leituraArquivo
+
+    public static void tratamentoCasos(BufferedReader leitor) throws Exception {
+
+        // BufferedReader leitor = new BufferedReader(new InputStreamReader(System.in));
+        String entrada = "";
+        int count = 0;
+        String casos = leitor.readLine();
+        while (count < Integer.parseInt(casos)) {
+
+            entrada = leitor.readLine();
+
+            String[] operacao = entrada.split(" ");
+
+            if (operacao[0].charAt(0) == 'I') {
+
+                if (operacao[0].charAt(1) == 'I') {
+                    ply = new Player();
+                    tratamentoString(leituraArquivo(operacao[1]));
+
+                    list.inserirInicio(ply);
+
+                } // end (II) inserir inicio
+                else if (operacao[0].charAt(1) == 'F') {
+
+                    ply = new Player();
+                    tratamentoString(leituraArquivo(operacao[1]));
+
+                    list.inserirFim(ply);
+
+                } // end (IF) inserir fim
+                else {
+
+                    ply = new Player();
+                    tratamentoString(leituraArquivo(operacao[2]));
+
+                    list.inserir(ply, Integer.parseInt(operacao[1]));
+
+                } // end (I*) inserir em qualquer lugar
+
+            } // end if insercao
+
+            if (operacao[0].charAt(0) == 'R') {
+
+                if (operacao[0].charAt(1) == 'I') {
+                    Player temp = list.removerInicio();
+                    System.out.println("(R) "+ temp.getNome());
+                } // end (RI) remover inicio
+                else if (operacao[0].charAt(1) == 'F') {
+
+                    Player temp = list.removerFim();
+                    System.out.println("(R) "+ temp.getNome());
+                } // end (RF) remover fim
+                else {
+                    Player temp = list.remover(Integer.parseInt(operacao[1]));
+                    System.out.println("(R) "+ temp.getNome());
+                } // end (R*) Remover local selecionado
+
+            } // end if remocao
+            count++;
+
+        } // end while
+
+    }// end tratamentoCasos
+
+    public static void main(String[] args) throws Exception {
+
         String nomeArq = "";
+        String casos = "";
         BufferedReader leitor = new BufferedReader(new InputStreamReader(System.in));
-        
-         nomeArq = leitor.readLine();
 
-        while(nomeArq.equals("FIM") != true ){
-            ply[coutPlayers] = new Player();
-            tratamentoString( leituraArquivo(nomeArq) );
-             Imprimir();
-             coutPlayers += 1;
+        nomeArq = leitor.readLine();
+
+        while (nomeArq.equals("FIM") != true) {
+            ply = new Player();
+            tratamentoString(leituraArquivo(nomeArq));
+            list.inserirFim(ply);
+
             nomeArq = leitor.readLine();
-        }//end while
+        } // end while
 
-    }//end main
+        tratamentoCasos(leitor);
 
-}//class
+        list.mostrar();
+
+    }// end main
+
+}// class Main
